ea03481a50f72509122e2cda68c075e21db45ae2
Matthew Wire <devel@mrwire.co.uk>
2017-11-15 19:10:18 +0000

    If paypal pro and paypal are enabled display Paypal as selector for paypal std

--- README.md
+++ README.md
@@ -4,3 +4,4 @@ Tweaks for republic.org.uk
 ## Contribution Pages
 * Hide the Paypal Pro button and text (css).
 * Rename "Credit Card Information" to "Card Information" (jquery).
+* If both Paypal Pro (ppid=13) and Paypal (ppid=1) are enabled, display "Paypal" as selector for paypal std.
--- js/contribution.js
+++ js/contribution.js
@@ -3,4 +3,11 @@ CRM.$(function() {
   if (cardInfoLabel) {
     cardInfoLabel.html('Card Information');
   }
+
+  // If we have both paypal pro (ppid=13) and paypal std (ppid=1) rename paypal std to "Paypal"
+  if (CRM.$('#CIVICRM_QFID_13_payment_processor_id')) {
+    if (CRM.$('#CIVICRM_QFID_1_payment_processor_id')) {
+      CRM.$('label[for="CIVICRM_QFID_1_payment_processor_id"]').html('Paypal');
+    }
+  }
 });
