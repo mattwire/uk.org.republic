d19396120d39c47d598999882c4d715318df784f
Matthew Wire <mjw@mjwconsult.co.uk>
2017-12-14 00:34:48 +0000

    Update payment processor IDs

--- js/contribution.js
+++ js/contribution.js
@@ -6,8 +6,8 @@ CRM.$(function() {
       cardInfoLabel.html('Card Information');
     }
 
-    // If we have both paypal pro (ppid=13) and paypal std (ppid=1) rename paypal std to "Paypal"
-    if (CRM.$('#CIVICRM_QFID_13_payment_processor_id')) {
+    // If we have both paypal pro (ppid=5) and paypal std (ppid=1) rename paypal std to "Paypal"
+    if (CRM.$('#CIVICRM_QFID_5_payment_processor_id')) {
       if (CRM.$('#CIVICRM_QFID_1_payment_processor_id')) {
         CRM.$('label[for="CIVICRM_QFID_1_payment_processor_id"]')
           .html('Paypal');
